2c4f423f8de8f55759c232e6ca55ce4824e78ad3
Daniel Nelson <dan.nelson001@gmail.com>
2017-07-22 17:38:11 -0700

    I hope this wasn't my first commit

---
+++ includes/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnelson <dnelson@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/22 15:46:10 by dnelson           #+#    #+#             */
+/*   Updated: 2017/07/22 17:37:40 by dnelson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include "libft.h"
+# include <stdarg.h>
+
+typedef	struct	s_flags
+{
+	char		c;
+	char		
