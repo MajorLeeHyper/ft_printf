14521a7c69f624a05de9a1849c096ad69afec258
Daniel Nelson <dan.nelson001@gmail.com>
2017-07-25 22:34:31 -0700

    created an itoa_base with features that make it modular for printf, I don't see this one being useful outside of printf though

---
+++ srcs/pf_itoa_base.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pf_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnelson <dnelson@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/25 22:20:34 by dnelson           #+#    #+#             */
+/*   Updated: 2017/07/25 22:24:44 by dnelson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		ft_intlen_base(uintmax_t num, size_t base)
+{
+	int		len;
+
+	len = 0;
+	if (num == 0)
+		return (1);
+	while (num)
+	{
+		num /= base;
+		++len;
+	}
+	return (len);
+}
+
+char	*pf_itoa_base(uintmax_t num, char *base_chars, size_t base)
+{
+	char	*str;
+	int		len;
+
+	len = ft_intlen_base(num, base);
+	if ((str = ft_strnew(len--)) == NULL)
+		return (NULL);
+	if (num == 0)
+		str[0] = base_chars[0];
+	while (num)
+	{
+		str[len--] = base_chars[(num % base)];
+		num /= base;
+	}
+	return (str);
+}
